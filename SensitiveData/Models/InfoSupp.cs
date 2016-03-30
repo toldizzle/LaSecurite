81ce95c3b1f3c72b1302a9e376216d931fb80f46
Toldizzle <toldizzle@hotmail.com>
2016-03-30 11:11:57 -0400

    Init

---
+++ SensitiveData/Models/InfoSupp.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace SensitiveData.Models
+{
+    public class InfoSupp
+    {
+        [Key]
+        public string ID { get; set; }
+        public string Nom { get; set; }
+        public string Prenom { get; set; }
+        [Required, CreditCard]
+        public string NumeroCarte { get; set; }
+        public string Adresse { get; set; }
+    }
+}
